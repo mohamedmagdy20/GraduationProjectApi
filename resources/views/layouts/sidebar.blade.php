7b993dd4ef552bf5eaf13b15d27c90197492b43b
Noor285 <nm1701037@gmail.com>
2023-05-07 20:45:35 +0300

    tips

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -184,33 +184,6 @@
         </li>
 
 
-
-          <li class="nav-item">
-            <span
-              class="nav-link  collapsed  d-flex justify-content-between align-items-center"
-              data-bs-toggle="collapse" data-bs-target="#submenu-Login-history">
-              <span>
-                <span class="sidebar-icon">
-                  <i class="fa fa-users"></i>
-                </span>
-                <span class="sidebar-text">Login History</span>
-              </span>
-              <span class="link-arrow">
-                <svg class="icon icon-sm" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-              </span>
-            </span>
-            <div class="multi-level collapse " role="list"
-              id="submenu-Login-history" aria-expanded="false">
-              <ul class="flex-column nav">
-                <li class="nav-item">
-                  <a class="nav-link"
-                    href="{{route('loginHisory.index')}}">
-                    <span class="sidebar-text">login history</span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </li>
         {{-- end of side bar --}}
 
 
@@ -292,6 +265,13 @@
               </li>  --}}
 
 
+                <li class="nav-item">
+                  <a class="nav-link"
+                    href="{{route('loginHisory.index')}}">
+                    <span class="sidebar-text">login history</span>
+                  </a>
+                </li>
+
             </ul>
           </div>
         </li>
@@ -306,6 +286,16 @@
               <span class="mt-1 ms-1 sidebar-text">Appointment Times</span>
             </a>
           </li>
+
+          <li class="nav-item">
+            <a href="{{route('tips.index')}}" class="nav-link d-flex align-items-center">
+              <span class="sidebar-icon">
+                <i class="fa-solid fa-calendar-days"></i>
+              </span>
+              <span class="mt-1 ms-1 sidebar-text">Tips</span>
+            </a>
+          </li>
+
         {{-- end of side bar --}}
         <li role="separator" class="dropdown-divider mt-4 mb-3 border-gray-700"></li>
 
